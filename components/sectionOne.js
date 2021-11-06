0e578942893841b599e0ed6a3e811f9c3e6c6dd7
fufudsby <fufudsby@gmail.com>
2021-11-06 07:13:32 +0700

    txt

--- components/sectionOne.js
+++ components/sectionOne.js
@@ -53,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   carouselItem: {
-    textAlign: 'left',
+    textAlign: 'justify',
     letterSpacing: 0.8,
     padding: theme.spacing(3, 0),
     '& .MuiTypography-root': {
@@ -105,16 +105,23 @@ const SectionOne = () => {
             showStatus={false}
           >
             <div className={classes.carouselItem}>
-              <Typography variant="body2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Typography>
-              <Typography variant="body2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+              <Typography variant="body2"><strong>To: Hana</strong></Typography>
+              <Typography variant="body2">Kaulah istriku, yang percaya pada hal-hal sepele dan paling remeh dari mimpi-mimpiku. Yang tidak peduli pada hal-hal yang disanjung orang lain dariku, tapi begitu memperhatikan hal-hal yang tak dilihat orang lain dari diriku. “Orang lain mana tahu kalau kamu suka joget-joget waktu mandi,” katamu suatu hari, “Mereka juga nggak tahu kalau ambil baju selalu bikin berantakan dan nggak nutup lemari,”</Typography>
+              <Typography variant="body2">Kemudian kamu menutup lemari baju, sembari mengomel “Hmmm..  Kebiasaan.. Latif Hendra Sukmana, angel nemen dikandani,”. Aku masih masih mengeringkan rambut saat itu, saat kamu bertanya, “Hari ini mau dibuatin sarapan apa?”. Kepalaku menyembul dari dalam kamar, melihatmu yang sedang sibuk merapikan tumpukan baju. “Terserah,” jawabku.</Typography>
             </div>
             <div className={classes.carouselItem}>
-              <Typography variant="body2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</Typography>
-              <Typography variant="body2">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</Typography>
+              <Typography variant="body2">Saat orang lain memuji hal yang tampak di permukaan, mereka tidak pernah memperhatikan hal-hal yang membuat semua itu ada. Karena semua yang kamu lakukan padahal hal lainnya, yang lebih hebat dari semua itu.</Typography>
+              <Typography variant="body2">Kaulah istriku, yang selalu menatapku dengan cara yang berbeda dari cara orang lain memandangku. Yang memperlakukanku tidak seperti orang lain memperlakukannya. Yang berbicara padaku dengan bahasa yang tidak diucapkan siapapun lagi di dunia ini. Yang percaya pada semua mimpi-mimpiku, sebodoh apapun itu.</Typography>
             </div>
             <div className={classes.carouselItem}>
-              <Typography variant="body2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Typography>
-              <Typography variant="body2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
+              <Typography variant="body2"><strong>To: Mas Latif</strong></Typography>
+              <Typography variant="body2">“Beruntung banget ya kamu, Han. Hubungannya seru banget. Ada saja yang bikin hidup suasana,” kata seorang temanku. Kadang aku juga merasa sebel kalau ada yang bilang begitu. Mereka tidak tahu saja bahwa kamu itu seringkali sangat menyebalkan. Mereka juga tidak tahu saja bahwa terkadang hubungan kita pun seringkali penuh drama. Begitulah risiko seorang anak tunggal yang menikahi anak laki-laki pertama sekaligus cucu pertama di keluarganya.</Typography>
+              <Typography variant="body2">Mereka yang mengira kamu sering menggombaliku setiap hari, tidak pernah tahu bahwa caramu bicara kepadaku lebih sering tanpa tata bahasa yang sempurna. Misalnya, “Yang, jam tangan,” itu artinya kamu bertanya di mana kacamatamu. Atau “Yang, aku budrek,” itu artinya kamu mau minta waktu me time dengan bermain PS4 tanpa diganggu.</Typography>
+            </div>
+            <div className={classes.carouselItem}>
+              <Typography variant="body2">Dan sering juga sambil menunjuk sebuah barang, kamu bilang “Yang, itunya tolong anuin,” yang artinya kamu minta aku untuk melakukan sesuatu, entah men-charge baterai handphone atau mengambilkan sesuatu. Meskipun kadang-kadang kamu cuma ketawa-ketawa saat aku tegur berulang kali, tapi ikutan marah kalau aku cuekin. Aku tidak apa-apa. Aku tahu pengorbanan dan hal yang telah kamu lalui lebih berat dariku.</Typography>
+              <Typography variant="body2">Kalau kamu mau tahu, sejak jadi istrimu banyak hal yang harus aku sesuaikan dalam hidupku, karena banyak kekacauan terjadi di sana. Tiba-tiba aku harus berdoa ribuan kali lebih banyak, sujud lebih lama, seringkali menangis diam-diam, khawatir berlebihan. Karena aku takut hal buruk menimpamu atau ketidakberuntungan merundungmu.</Typography>
+              <Typography variant="body2">Hm.. seandainya suami—suami tahu, pikiran dan perasaan seorang istri kadang-kadang sekacau itu. Tapi tetap saja, mencintaimu adalah kekacauan paling indah di hidupku.</Typography>
             </div>
           </Carousel>
         </Box>
